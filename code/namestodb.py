c5d87bde2536be875d563806ad44c8dfcc5bc217
Francis Irving <francis@flourish.org>
2013-04-02 17:13:51 +0100

    Cope with UTF-8 names, including updating fixtures to test it

--- code/namestodb.py
+++ code/namestodb.py
@@ -2,13 +2,14 @@
 # namestodb.py
 
 import sys
-
 import scraperwiki
+import codecs
 
 """Take a list of names on stdin (one name per line),
 store each name in a sqlite table."""
 
 def main():
+    sys.stdin = codecs.getreader('utf-8')(sys.stdin)
     for id,row in enumerate(sys.stdin):
         row = row.strip()
         scraperwiki.sql.save(['id'], dict(name=row, id=id), table_name="source")
