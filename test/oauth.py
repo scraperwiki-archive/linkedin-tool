7e6b5737eaaf24b3de75cc704ccd53e4c0b2d479
Francis Irving <francis@flourish.org>
2013-07-12 14:41:01 +0100

    No more x.scraperwiki.com

--- test/oauth.py
+++ test/oauth.py
@@ -7,11 +7,11 @@ browser = Browser(os.environ.get('SPLINTER_BROWSER', 'firefox'))
 
 def setUp():
   browser.cookies.delete()
-  browser.visit('http://x.scraperwiki.com/dataset/fello3q/settings')
+  browser.visit('http://scraperwiki.com/dataset/fello3q/settings')
   browser.find_by_css('#username').fill(os.environ['X_INT_USER'])
   browser.find_by_css('#password').fill(os.environ['X_INT_PASS'])
   browser.find_by_css('#login').click()
-  browser.visit('http://x.scraperwiki.com/dataset/fello3q/settings')
+  browser.visit('http://scraperwiki.com/dataset/fello3q/settings')
 
 def ensure_i_am_on_the_settings_page():
   assert browser.is_text_present('Settings', wait_time=10)
